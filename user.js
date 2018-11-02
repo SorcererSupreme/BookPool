bd39e436045452279febd2f71264d0d1f281d49b
Siddhant Jain <siddhantjain455@gmail.com>
2018-11-02 10:33:57 +0530

    deleted user.js as userDAO is created

--- user.js
+++
@@ -1,8 +0,0 @@
-module.exports.User  = {
-	findOrCreate: function(appId, cb){
-		console.log("A new user is created! with facebookid = " + appId.facebookId.id);
-		var profile = appId.facebookId;
-		console.log("profile is ",JSON.stringify(profile))
-		return cb(null,profile);
-	}
-}
